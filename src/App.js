d93e388e7396ed4e1798620320d719b73631831b
baptiste giorgi <badbat_69@hotmail.com>
2025-11-21 15:35:17 +0100

    learning of the react state and how to use them

--- src/App.js
+++ src/App.js
@@ -1,41 +1,75 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/marker';
 
-function App() {
-  const flat = {
-    "id": "1",
-    "name": "Charm at the Steps of the Sacré-Cœur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
-    "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.34689
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: [],
+      // State to keep track of the selected flat
+      selectedFlat: null
+    };
+  }
 
-  const flats = [flat, flat, flat, flat, flat];
-  // fetch('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json')
-  //   .then(response => response.json())
-  //   .then(data => flats.push(data));
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ flats: data });
+      });
+  }
 
+  // Method to update the selectedFlat in the state
+  selectFlat = (flat) => {
+    this.setState({ selectedFlat: flat });
+  }
 
-  return (
-    <div className='app'>
-      <div className='main'>
-        <div className='search'>
+  render() {
+    let center = {
+      lat: 48.8566, lng: 2.3522
+    };
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      };
+    }
 
+    return (
+      <div className='app'>
+        <div className='main'>
+          <div className='search'>
+
+          </div>
+          <div className='flats '>
+            {this.state.flats.map((flat) => {
+              return <Flat
+              flat={flat}
+              key={flat.id}
+              // We pass down the selectFlat method as a prop
+              selectFlat={this.selectFlat} />
+            })}
+          </div>
+          </div>
+        <div className='map'>
+          <GoogleMapReact
+            center={center}
+            zoom={14}>
+              {this.state.flats.map((flat) => {
+                return <Marker
+                  lat={flat.lat}
+                  lng={flat.lng}
+                  text={flat.price + flat.priceCurrency}
+                  key={flat.id} />
+              })}
+          </GoogleMapReact>
         </div>
-        <div className='flats '>
-          {flats.map((flat) => {
-            return <Flat flat={flat} key={flat.id} />
-          })}
-        </div>
-        </div>
-      <div className='map'>
-
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/components/flat.js
+++ src/components/flat.js
@@ -2,13 +2,18 @@ import React from "react";
 import './flat.css';
 
 class Flat extends React.Component {
+  // When clicking on a flat, we call the selectFlat method received as a prop
+  handleClick = () => {
+    this.props.selectFlat(this.props.flat);
+  }
   render() {
     const title = `${this.props.flat.price}${this.props.flat.priceCurrency} - ${this.props.flat.name}`
     const style = {
       backgroundImage: `url('${this.props.flat.imageUrl}')`
     }
     return (
-      <div className="flat">
+      // On click, we call the handleClick method
+      <div className="flat" onClick={this.handleClick}>
         <div className="flat-picture" style={style}>
         </div>
         <div className="flat-title">
